7d86062416cbff30bd319c54c7c643c0b92dcc2f
Krittawat Wisedsuwon <krittawat.code@gmail.com>
2020-08-07 01:31:48 +0700

    first commit

---
+++ src/main/java/kriwis/springframework/kriwisbrewery/KriwisBreweryApplication.java
@@ -0,0 +1,13 @@
+package kriwis.springframework.kriwisbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KriwisBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KriwisBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kriwis/springframework/kriwisbrewery/KriwisBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package kriwis.springframework.kriwisbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KriwisBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
