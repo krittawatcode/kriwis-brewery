d8f7aa03112fe62d8e2696e4cc47738c746c0f4b
Krittawat Wisedsuwon <krittawat.w@ngerntidlor.com>
2020-08-07 10:26:20 +0700

    add put and delete beer

--- src/main/java/kriwis/springframework/kriwisbrewery/services/BeerServiceImpl.java
+++ src/main/java/kriwis/springframework/kriwisbrewery/services/BeerServiceImpl.java
@@ -1,12 +1,15 @@
 package kriwis.springframework.kriwisbrewery.services;
 
 import kriwis.springframework.kriwisbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
+
     @Override
     public BeerDto getBeerById(UUID beerId) {
         return BeerDto.builder().id(UUID.randomUUID())
@@ -15,5 +18,19 @@ public class BeerServiceImpl implements BeerService {
                 .build();
     }
 
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        // todo impl would add real update
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer ...");
+    }
 
 }
--- src/main/java/kriwis/springframework/kriwisbrewery/web/controller/BeerController.java
+++ src/main/java/kriwis/springframework/kriwisbrewery/web/controller/BeerController.java
@@ -4,12 +4,10 @@ import kriwis.springframework.kriwisbrewery.services.BeerService;
 
 import kriwis.springframework.kriwisbrewery.web.model.BeerDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,6 +24,29 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
+        BeerDto saveDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo add hostname to url
+        headers.add("Location", "/api/v1/beer" + saveDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public  ResponseEntity handleUpdate(@PathVariable UUID beerId,@RequestBody BeerDto beerDto){
+        beerService.updateBeer(beerId, beerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable UUID beerId){
+        beerService.deleteById(beerId);
     }
 
 }
