eaf6a05917a33aa65452d89796d413d63eab9792
Krittawat Wisedsuwon <krittawat.w@ngerntidlor.com>
2020-08-07 10:43:37 +0700

    add customer put post delete

--- src/main/java/kriwis/springframework/kriwisbrewery/services/CustomerServiceImpl.java
+++ src/main/java/kriwis/springframework/kriwisbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package kriwis.springframework.kriwisbrewery.services;
 
 import kriwis.springframework.kriwisbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService{
     @Override
@@ -14,4 +16,21 @@ public class CustomerServiceImpl implements CustomerService{
                 .name("Sixt LightMaker")
                 .build();
     }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // todo impl would add real update
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting customer ...");
+    }
+
+
 }
